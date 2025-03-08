97e9c070929b90927362b3ff204e4ee3587e4d04
Mathewski77 <mlacsina23-0408@cca.edu.ph>
2025-03-08 16:02:37 +0800

    Create Readme.md

---
+++ Midterm task 2/Readme.d
@@ -0,0 +1,32 @@
+### <ins>Midterm Lab Task 1 - Data Cleaning and Preparation using Excel<ins>
+For this task we are given a Flat data from Excel (See rawfile) and we are Task to perform Data CLeaning and Preparation
+
+### STEP 1 Data Cleaning process
+<hr>
+-Load the raw file <BR>
+-Fit Column and row width and height <BR>
+-TRIM extra spaces <BR>
+-Remove NULL values <BR>
+-Remove Duplicates <BR>
+
+### STEP 2 Normalization
+<hr>
+-Performed 1NF <BR>
+-Perform Table split using 2NF <BR>
+-Perform 3 NF <BR>
+-Mapped the Normalized tables as a Physical Data Model <BR>
+
+### STEP 3 Here's the screenshot of my output before I started data cleaning (See screenshot)
+
+<img src="" width="500" height="300"> <br>
+
+### STEP 4 Here's the screenshot of my output after I started data cleaning (See screenshot)
+
+
+
+
+
+
+
+
+
